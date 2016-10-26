641454b5f23c16bf6d94ac223765a662945ca83b
Hibryda <barmesser@gmail.com>
2016-10-26 08:12:59 +0200

    Fixed issues with settings, again /wrong file/

--- hub/ether.settings.js
+++
@@ -1,7 +0,0 @@
-# Config file
-
-There should be a config file attached to app with structure as below. It should be named "ether.settings.js"
-
-module.exports = {
-  etherServerRpc:'http://10.0.0.8:8545',
-}
