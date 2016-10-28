2a298e93bb452f1b29c8500e306b5b6abb2f16bf
Dan Phifer <dan.phifer@gmail.com>
2016-10-28 09:44:12 +0800

    * Adding a simplified user badge for my version of the UI until the user account mgmt is sorted out.

---
+++ interface/elements/msc-subcomponents/msc-simple-user-badge/msc-simple-user-badge.js
@@ -0,0 +1,17 @@
+Polymer({
+    is: 'msc-simple-user-badge',
+    ready:function(){
+      mscIntf.userDetails = {register:this,prop:'userDetails'};
+      mscIntf.toolSettings.userImagePath = {register:this,prop:'usersImageDir'};
+    },
+    properties: {
+      userDetails: Object,
+      usersImageDir: String,
+    },
+    menuPick: function(ev) {
+      if (ev.target.selected=='loo') mscIntf.fnPool('login','logoutUser');
+      if (ev.target.selected=='acc') document.querySelector('app-account-create-confirm-dialog').open()
+      if (ev.target.selected=='set') document.querySelector('app-user-settings-view').open()
+      ev.target.selected = null;
+    },
+})
