a1f157e0f1f5b91a4eda83bef53f118d82cf6e4a
Dan Phifer <dan.phifer@gmail.com>
2016-11-14 22:06:44 +0800

    * Updating linux setup script to remove Windows line endings and add geth and ipfs to the list of things that need to be executable

--- bin/linux-setup.sh
+++ bin/linux-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-# add required package for nwjs
-apt install libnss3
+# add required package for nwj
+sudo apt install libnss3
 
 # Make the application executable
 chmod +x Musicoin-client
+chmod +x bin/geth/geth
+chmod +x bin/go-ipfs/ipfs
